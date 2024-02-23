7354eec867c8da9d46128e6b3a02636bdca6ac04
Marcell <havlikmarcell@gmail.com>
2024-02-23 17:29:35 +0100

    removing types

--- src/ExtendableStruct.jl
+++ src/ExtendableStruct.jl
@@ -1,40 +1,40 @@
 module ExtendableStruct
 
-using InitLoadableStruct: InitableLoadable
+using InitLoadableStruct: InitableLoadable, load_data!, init
 
 export extend!
 
 abstract type Extendable <: InitableLoadable end
 
 ######## Core functions
-extend!(obj::T,c_obj::T)                        where T <: Extendable = merge(data_before(obj, c_obj), c_obj, data_after(c_obj, obj))
-merge(before::T,      cached::T,after::T)       where T <: Extendable = append(append(before,cached),after), true
-merge(before::Nothing,cached::T,after::T)       where T <: Extendable = append(cached,after), true
-merge(before::T,      cached::T,after::Nothing) where T <: Extendable = append(before,cached), true
-merge(before::Nothing,cached::T,after::Nothing) where T <: Extendable = cached, false
+extend!(obj::T,c_obj::T)                        where T = merge(data_before(obj, c_obj), c_obj, data_after(c_obj, obj))
+merge(before::T,      cached::T,after::T)       where T = append(append(before,cached),after), true
+merge(before::Nothing,cached::T,after::T)       where T = append(cached,after), true
+merge(before::T,      cached::T,after::Nothing) where T = append(before,cached), true
+merge(before::Nothing,cached::T,after::Nothing) where T = cached, false
 
 
 ######### Optionalble Redefineable Interfaces
 # If you want 1 preallocation and merge all the three there at once
 # is_same(o1::T, o2::T)       where T = (throw("Unimplemented is_same(...)"); return o1.config == o2.config && o1.fr == o2.fr && o1.to == o2.to) 
-append(before::T, after::Nothing) where T <: Extendable = before 
-append(before::Nothing, after::T) where T <: Extendable = after
+append(before::T, after::Nothing) where T = before 
+append(before::Nothing, after::T) where T = after
 
-data_before(obj::T, c::T)         where T <: Extendable = need_data_before(obj,c) ? load_data!(init_before_data(obj,c)) : nothing
-data_after(c::T,  obj::T)         where T <: Extendable = need_data_after(obj,c)  ? load_data!(init_after_data(obj,c))  : nothing
+data_before(obj::T, c::T)         where T = need_data_before(obj,c) ? load_data!(init_before_data(obj,c)) : nothing
+data_after(c::T,  obj::T)         where T = need_data_after(obj,c)  ? load_data!(init_after_data(obj,c))  : nothing
 
 
 ######### REDEFINE
 # Concat two data with same config.
-append(before::T, after::T)      where T <: Extendable = throw("Implement the merging process, how do you concat two $T")
+append(before::T, after::T)      where T = throw("Implement the merging process, how do you concat two $T")
 
 # Do we need new data (in front/after) of our current data?
-need_data_before(obj::T, c::T)   where T <: Extendable = obj.fr < c.fr
-need_data_after(obj::T,  c::T)   where T <: Extendable = c.to < obj.to
+need_data_before(obj::T, c::T)   where T = obj.fr < c.fr
+need_data_after(obj::T,  c::T)   where T = c.to < obj.to
 
 # Configure and init the (before/after) object that is able to download the right data with load_data
-init_before_data(obj::T, c::T)   where T <: Extendable = init(T, obj.fr, c.fr, obj.config)
-init_after_data(obj::T,  c::T)   where T <: Extendable = init(T, c.to, obj.to, obj.config)
+init_before_data(obj::T, c::T)   where T = init(T, obj.config, obj.fr, c.fr)
+init_after_data(obj::T,  c::T)   where T = init(T, obj.config, c.to, obj.to)
 
 
 
