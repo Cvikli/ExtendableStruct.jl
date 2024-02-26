82ab9642a8bd2b2d8d78dbe3050537c17c96fdcb
Marcell <havlikmarcell@gmail.com>
2024-02-26 16:19:03 +0100

    More flexibility from types by removing the "same type" condition

--- src/ExtendableStruct.jl
+++ src/ExtendableStruct.jl
@@ -8,21 +8,21 @@ export extend!
 abstract type Extendable <: InitableLoadable end
 
 ######## Core functions
-extend!(obj::T,c_obj::T)                        where T = merge(data_before(obj, c_obj), c_obj, data_after(c_obj, obj))
-merge(before::T,      cached::T,after::T)       where T = append(append(before,cached),after), true
-merge(before::Nothing,cached::T,after::T)       where T = append(cached,after), true
-merge(before::T,      cached::T,after::Nothing) where T = append(before,cached), true
-merge(before::Nothing,cached::T,after::Nothing) where T = cached, false
+extend!(obj,c_obj)                           = merge(data_before(obj, c_obj), c_obj, data_after(c_obj, obj))
+merge(before,         cached,after)          = append(append(before,cached),after), true
+merge(before::Nothing,cached,after)          = append(cached,after), true
+merge(before,         cached,after::Nothing) = append(before,cached), true
+merge(before::Nothing,cached,after::Nothing) = cached, false
 
 
 ######### Optionalble Redefineable Interfaces
 # If you want 1 preallocation and merge all the three there at once
 # is_same(o1::T, o2::T)       where T = (throw("Unimplemented is_same(...)"); return o1.config == o2.config && o1.fr == o2.fr && o1.to == o2.to) 
-append(before::T, after::Nothing) where T = before 
-append(before::Nothing, after::T) where T = after
+append(before,          after::Nothing) = before 
+append(before::Nothing, after)          = after
 
-data_before(obj::T, c::T)         where T = need_data_before(obj,c) ? load_data!(init_before_data(obj,c)) : nothing
-data_after(c::T,  obj::T)         where T = need_data_after(obj,c)  ? load_data!(init_after_data(obj,c))  : nothing
+data_before(obj, c)         = need_data_before(obj,c) ? load_data!(init_before_data(obj,c)) : nothing
+data_after(c,  obj)         = need_data_after(obj,c)  ? load_data!(init_after_data(obj,c))  : nothing
 
 
 ######### REDEFINE
